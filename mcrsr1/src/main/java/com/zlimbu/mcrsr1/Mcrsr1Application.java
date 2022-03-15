bb142c0e4054c6c4af13e4f56c6cc72baf1fd2a1
anglal <lalshwarlimbu@gmail.com>
2022-03-14 17:32:45 -0700

    adding another rest api

---
+++ mcrsr1/src/main/java/com/zlimbu/mcrsr1/Mcrsr1Application.java
@@ -0,0 +1,13 @@
+package com.zlimbu.mcrsr1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mcrsr1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mcrsr1Application.class, args);
+	}
+
+}
---
+++ mcrsr1/src/test/java/com/zlimbu/mcrsr1/Mcrsr1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zlimbu.mcrsr1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mcrsr1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
